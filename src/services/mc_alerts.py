30a110d11fa42342cd858346288be2b42a99b80a
Matthew Klawitter <mtklaw153@gmail.com>
2021-10-11 19:21:06 -0400

    Changed handling on response triggers

--- src/services/mc_alerts.py
+++ src/services/mc_alerts.py
@@ -5,25 +5,25 @@ class MCAlerts:
     def __init__(self, bridge, room):
         self.bridge = bridge
         self.room = room
-        self.players = {}
+        self.players = 0
         self.server = MinecraftServer("mc.seafarers.cafe", 25565)
 
     async def task(self):
-        status = self.server.status()
+        try:
+            status = self.server.status()
 
-        # TODO: Solve race condition if multiple players connect/disconnect within same ping period
-        if not len(status.players.sample) == len(self.players):
-            if len(status.players.sample) > len(self.players):
-                message = "The following players connected:\n"
-                for p in status.players.sample:
-                    if p not in self.players:
-                        message += "{}\n".format(p.name)
-                self.players = status.players.sample
-                await self.bridge.send_message(self.room, message)
+            if status.players.sample is not None:
+                if not len(status.players.sample) == self.players:
+                    self.players = len(status.players.sample)
+                    message = "Minecraft Server - connected users:\n"
+
+                    for p in status.players.sample:
+                        message += "{}\t".format(p.name)
+
+                    await self.bridge.send_message(self.room, message)
             else:
-                message = "The following players disconnected:\n"
-                for p in self.players:
-                    if p not in status.players.sample:
-                        message += "{}\n".format(p.name)
-                self.players = status.players.sample
-                await self.bridge.send_message(self.room, message)
+                if self.players > 0:
+                    self.players = 0
+                    await self.bridge.send_message(self.room, "Minecraft Server - no connected users detected.")
+        except Exception as e:
+            print(e)
