50ff90a39cfa56b5c8a84c2825b08e499a27ba9f
Mark Powers <mark@marks.kitchen>
2021-10-23 16:38:39 -0500

    Add celebrate plugin

---
+++ src/plugins/celebrate.py
@@ -0,0 +1,31 @@
+import heapq
+import bs4 as bs
+import re
+import urllib.request
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+import requests
+from lxml.html import fromstring
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+class CelebratePlugin:
+    def get_commands(self):
+        return {}
+
+    def get_name(self):
+        return "Celebrate"
+
+    def get_help(self):
+        return "Sends a fireworks\n"
+
+    async def message_listener(self, message):
+        if (re.search(r'\bwow\b', message.message, flags=re.IGNORECASE) or
+                re.search(r'\bnice\b', message.message, flags=re.IGNORECASE) or
+                re.search(r'\bawesome\b', message.message, flags=re.IGNORECASE) or
+                re.search(r'\bcongrats\b', message.message, flags=re.IGNORECASE)):
+            await message.bridge.send_message(message.room_id, text="wow!", msg_type="nic.custom.fireworks")
