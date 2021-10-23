e5bf91f9f44e7be16b54551c80062be727b2ee8f
Matthew Klawitter <mtklaw153@gmail.com>
2021-10-22 21:15:00 -0400

    Added exception handling for lack of data

--- src/plugins/preview.py
+++ src/plugins/preview.py
@@ -30,55 +30,59 @@ class PreviewPlugin:
             await message.bridge.send_html(message.room_id, response)
 
     def get_summary(self, url):
-        scraped_article = urllib.request.urlopen(url)
-        article = scraped_article.read()
-        parsed_article = bs.BeautifulSoup(article, 'lxml')
+        try:
+            scraped_article = urllib.request.urlopen(url)
+            article = scraped_article.read()
+            parsed_article = bs.BeautifulSoup(article, 'lxml')
 
-        paragraphs = parsed_article.find_all('p')
-        article_text = ""
+            paragraphs = parsed_article.find_all('p')
+            article_text = ""
 
-        for p in paragraphs:
-            article_text += p.text
+            for p in paragraphs:
+                article_text += p.text
 
-        # Some preprocessing
-        article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-        article_text = re.sub(r'\s+', ' ', article_text)
+            # Some preprocessing
+            article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+            article_text = re.sub(r'\s+', ' ', article_text)
 
-        formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
-        formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+            formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+            formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
-        sentence_list = nltk.sent_tokenize(article_text)
+            sentence_list = nltk.sent_tokenize(article_text)
 
-        stopwords = nltk.corpus.stopwords.words('english')
+            stopwords = nltk.corpus.stopwords.words('english')
 
-        word_frequencies = {}
-        for word in nltk.word_tokenize(formatted_article_text):
-            if word not in stopwords:
-                if word not in word_frequencies.keys():
-                    word_frequencies[word] = 1
-                else:
-                    word_frequencies[word] += 1
+            word_frequencies = {}
+            for word in nltk.word_tokenize(formatted_article_text):
+                if word not in stopwords:
+                    if word not in word_frequencies.keys():
+                        word_frequencies[word] = 1
+                    else:
+                        word_frequencies[word] += 1
 
-        maximum_frequncy = max(word_frequencies.values())
+            maximum_frequncy = max(word_frequencies.values())
 
-        for word in word_frequencies.keys():
-            word_frequencies[word] = (word_frequencies[word] / maximum_frequncy)
+            for word in word_frequencies.keys():
+                word_frequencies[word] = (word_frequencies[word] / maximum_frequncy)
 
-        sentence_scores = {}
-        for sent in sentence_list:
-            for word in nltk.word_tokenize(sent.lower()):
-                if word in word_frequencies.keys():
-                    if len(sent.split(' ')) < 30:
-                        if sent not in sentence_scores.keys():
-                            sentence_scores[sent] = word_frequencies[word]
-                        else:
-                            sentence_scores[sent] += word_frequencies[word]
+            sentence_scores = {}
+            for sent in sentence_list:
+                for word in nltk.word_tokenize(sent.lower()):
+                    if word in word_frequencies.keys():
+                        if len(sent.split(' ')) < 30:
+                            if sent not in sentence_scores.keys():
+                                sentence_scores[sent] = word_frequencies[word]
+                            else:
+                                sentence_scores[sent] += word_frequencies[word]
 
-        summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
+            summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
 
-        pre = "<blockquote>"
-        post = "</blockquote>"
-        summary = ' '.join(summary_sentences)
-        
-        result = pre + summary + post
-        return result
+            pre = "<blockquote>"
+            post = "</blockquote>"
+            summary = ' '.join(summary_sentences)
+            
+            result = pre + summary + post
+            return result
+        except ValueError as e:
+            print(e)
+            return "<blockquote>No summary available</blockquote>"
