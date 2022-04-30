3a8a7c4710b6a330c280b764c434fb53be557636
Matthew Klawitter <mattklawitt@gmail.com>
2022-04-30 15:06:14 -0400

    Added a ton of content

--- src/plugins/ratio.py
+++ src/plugins/ratio.py
@@ -32,7 +32,7 @@ class RatioPlugin:
 
     def get_ratio(self):
         # Current data used for ratio generation
-        general = ["L", "Ratio", "You fell off"]
+        prefix = ["L", "Ratio"]
         insult = ["You done messed up", "Reaction score", "Upset", "You moron",
             "Percentile"]
         corporate = ["No synergy", "Touchbase", "HR'd",
@@ -43,25 +43,53 @@ class RatioPlugin:
             "En passant", "Reverse card", "Trap card", "King me"]
         rent = ["Over priced", "Landlorded", "Renters agreement", 
             "Rent to OWNed", "No water"]
-        hacker = ["Social engineered", "Wacky digits", "Agent Surefire'd"]
-        stock = ["Stonked", "Not financial advice", "Paper hands", "Bought the dip"]
+        hacker = ["Social engineered", "Wacky digits", "Agent Surefire'd", "Literally in your system", "No anti-virus"]
+        stock = ["Stonked", "Not financial advice", "Paper hands", "Bought the dip", "Went broke"]
         crypt = ["Gas priced", "Thanks for the NFT", "Pump and dump'd",
             "Screenshotted", "Fungible", "Right clicked"]
-        chef = ["Overcooked", "Toasted", "Burned", "Roasted"]
+        chef = ["Overcooked", "Toasted", "Burned", "Roasted", "Raw", "Unseasoned", "Eggless coffee"]
         elden = ["No Maidens", "Your sword has shoddy craftsmanship",
             "Your treasure hoard is empty", "No vigor"]
-        
-        ratio_list = [insult, corporate, coding, boardgame, rent, hacker, stock,
-            crypt, chef, elden]
+        dino = ["Extinct", "Fossilized", "Stuck in tar"]
+        cringe = ["Cringe", "Cope", "Ok boomer", "You are not just a clown you are the entire circus"]
+        rude = ["Didn't ask", "Don't care", "Skill issue"]
+        news = ["Fake news", "That's made up", "Old news bro", "So yesterday"]
+        reddit = ["Reddit silver", "No upvotes", "Go tell Reddit", "Log off", "You're going in my cringe compilation"]
+        pirate = ["Plundered", "Pillaged", "I found yer treasure", "Davy Jones don't even want the likes of ye in his locker", "Shipwrecked", "No booty", "All yer wenches be servicin' my crew"]
+        caveman = ["Me have bigger cave", "Me not care", "Small meat stick", "Touch rock", "Never breed", "You not only Ooga but entire Chakalaka"]
+        french = ["You are english", "Mind your own business", "Boil your bottoms", "You're the son of a silly person", "I blow my nose at you", "'So called'", "Your KNNH-Knights are silly", "Block me before I taunt you a second time"]
+        tf2 = ["Red spy in the base", "No medics", "Flag stolen", "I have more crates than you", "Not MANN enough"]
+        fish = ["Not enough bait", "Weak hook", "I catch more fish than you"]
+        windows = ["Windows user", "Incoming Windows update", "Bill-Gated", "Reinstall your OS"]
 
+        ratio_list = [insult, corporate, coding, boardgame, rent, hacker, stock,
+            crypt, chef, elden, dino, cringe, rude, news, reddit, pirate, caveman, french, tf2, fish, windows]
+
+        base = []
         data_set = random.choice(ratio_list)
 
         if (len(data_set) >= 3):
-            response0 = random.choice(general)
+            response0 = random.choice(prefix)
+            base.append(response0)
+            base.append(" + ")
+
             response1 = random.choice(data_set)
+            base.append(response1)
             data_set.remove(response1)
+            base.append(" + ")
+
             response2 = random.choice(data_set)
+            base.append(response2)
             data_set.remove(response2)
+            base.append(" + ")
+
             response3 = random.choice(data_set)
-            return "{} + {} + {} + {}".format(response0, response1, response2, response3)
+            base.append(response3)
+            data_set.remove(response3)
+
+            response = ""
+            for phrase in base:
+                response += "{}".format(phrase)
+
+            return response
         return None
