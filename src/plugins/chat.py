81fe67a14c6bb22f7c860aeae0ef54a36baf4d9c
Mark Powers <mark@marks.kitchen>
2023-08-27 16:16:31 -0500

    Add ai meme response.

--- src/plugins/chat.py
+++ src/plugins/chat.py
@@ -1,4 +1,9 @@
 import re, os, random
+import openai
+import json
+import urllib.request
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
 
 from asyncio import run
 
@@ -12,14 +17,47 @@ async def random_rip(message):
 async def no(message):
     await message.bridge.send_image(message.room_id, "/res/no.jpeg")
 
+async def send_ai_meme(message):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": "Output JSON with a caption field and image field. The caption should be the text of a meme relating to workplace humor. The image field should be a description of this meme's image."
+            },
+        ],
+        max_tokens=1000,
+        temperature=0.8,
+    )
+    j = json.loads(response.choices[0]["message"]["content"])
+
+    image = openai.Image.create(
+      prompt=j["image"],
+      n=2,
+      size="512x512"
+    )
+    urllib.request.urlretrieve(image["data"][0]["url"], "/tmp/gpt_image.jpg")
+
+
+    img = Image.open("/tmp/gpt_image.jpg")
+    font = ImageFont.truetype("/res/COMIC.TTF", 48)
+    draw = ImageDraw.Draw(img)
+    for i, line in enumerate(textwrap.wrap(j["caption"], width=20)):
+        draw.text((0, 50*i), line, fill='white', font=font, stroke_width=2, stroke_fill='black')
+
+    img.save("/res/out.jpg")
+    await message.bridge.send_image(message.room_id, "/res/out.jpg")
+
 responses = (
     ( r"\brip\b", [random_rip] ),
     ( r"no\s\w+\?", [no] ),
+    ( r"\bwork\b", [send_ai_meme] ),
+    ( r"\bworking\b", [send_ai_meme] ),
 )
 
 class ChatPlugin:
     def load(self, room, web_app, web_admin):
-        pass
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
 
     def get_commands(self):
         return {}
